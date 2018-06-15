ce54113dd33d23f8c458c97d0305eaa00274846b
Will Lee <will.i.lee@outlook.com>
2018-06-15 15:35:34 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  #returns number of songs in class
+  def self.count
+    @@count
+  end
+
+  #returns the number of distinct genres in class
+  def self.genres
+    distinct_genre_list = []
+    @@genres.each do |genre|
+      binding.pry
+      if @@genres.none?{|genre| genre}
+        distinct_genre_list << genre
+        binding.pry
+      end
+    end
+
+    return distinct_genre_list
+  end
+
+  #returns the number of distinct genres in class
+  def self.artists
+    distinct_artists_list = []
+      if @@artists.none?{|artist| artist}
+        distinct_artists_list << artist
+        binding.pry
+      end
+
+    return distinct_artists_list
+  end
+
+  def self.genre_count
+
+  end
+
+
+
+
+
+
+
+end
